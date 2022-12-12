732d2a125febe8abf82a131fc11e430ea5885f31
L4 <68635708+L42N@users.noreply.github.com>
2022-12-12 21:02:06 +0000

    Deleted Scala class import

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import Scala.csv_persistence;
+
 
 public class MainGUI {
     private JPanel MainPanel;
